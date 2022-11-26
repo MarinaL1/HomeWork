207cb16486b58cd293c9dd9a52f054b43077ec22
MarinaL1 <gorkunova.m@gmail.com>
2022-11-26 13:43:22 +0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    println("First side")
+    val length = readln().toInt()
+    println("Second side")
+    val width = readln().toInt()
+    println("Third side")
+    val height = readln().toInt()
+    println("Calculate volume")
+    var volume = length * width * height
+
+    println("Result: $volume")
+
+
+
+}
